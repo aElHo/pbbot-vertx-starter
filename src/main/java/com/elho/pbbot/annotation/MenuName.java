d49c4bd0bf8f86a804d791f0e3fafce3ce855b2b
yf.zhuang <1069974881@qq.com>
2022-04-11 01:04:33 +0800

    增加菜单说明注解

---
+++ src/main/java/com/elho/pbbot/annotation/MenuName.java
@@ -0,0 +1,17 @@
+package com.elho.pbbot.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zyf
+ * @Date 2022-04-11
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MenuName {
+    String value() default "";
+
+}
